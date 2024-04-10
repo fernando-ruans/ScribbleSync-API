ee46a50407c84255e9683e6aeea91c9fc639a0bc
Fernando Ruan <fernando_ruans@hotmail.com>
2024-04-10 10:13:53 -0300

    Carregando Imagens.

--- src/configs/upload.js
+++ src/configs/upload.js
@@ -1,9 +1,9 @@
-const { path } = require("path");
+const path = require("path");
 const multer = require("multer");
 const crypto = require("crypto")
 
 const TMP_FOLDER = path.resolve(__dirname, "..","..", "tmp");
-const UPLOADS_FOLDER = path.resolve(__dirname, "uploads");
+const UPLOADS_FOLDER = path.resolve(TMP_FOLDER, "uploads");
 
 const MULTER = {
   storage: multer.diskStorage({
