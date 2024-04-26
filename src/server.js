8d2c94e79cf50cc22de05411007c8c6c3ced15f9
Fernando Ruan <fernando_ruans@hotmail.com>
2024-04-26 17:21:49 -0300

    dotenv.

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require("dotenv/config");
 require("express-async-errors");
 const migrationsRun = require("./database/sqlite/migrations");
 const AppError = require("./utils/AppError");
@@ -33,7 +34,7 @@ app.use(( error, request, response, next) => {
   });
 });
  
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
 
