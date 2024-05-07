d47b387eab74f268f15ffd60e5be21d208910b41
Fernando Ruan <fernando_ruans@hotmail.com>
2024-05-07 09:34:00 -0300

    Utilizando o user repository.

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,13 +1,16 @@
 const { hash, compare } = require("bcrypt");
 const AppError = require("../utils/AppError");
+
+const UserRepository = require("../repositories/UserRepository");
 const sqliteConnection = require("../database/sqlite");
 
 class UsersController {
   async create(request, response) {
     const { name, email, password } = request.body;
 
-    const database = await sqliteConnection();
-    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+    const userRepository = new UserRepository();
+
+    const checkUserExists = await userRepository.findByEmail(email);
 
     if (checkUserExists) {
       throw new AppError("Este e-mail já está em uso.");
@@ -15,7 +18,7 @@ class UsersController {
 
     const hashedPassword = await hash(password, 8);
     
-    await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword]);
+    await userRepository.create({ name, email, password: hashedPassword});
 
     return response.status(201).json();
   }
